45ee683d28ff34332c99476b6bf2c9b23c6592d2
Glauco Azevedo <gazbr234@gmail.com>
2020-11-03 22:19:56 -0300

    Update texting.py
    add a missing import

--- texting.py
+++ texting.py
@@ -1,6 +1,7 @@
 import os 
 import re
 import pandas as pd
+from unicodedata import normalize
 
 # This module contains methods to make easier ETL steps on commons data analysis
 # cases such as pivoting, cleaning, formatting using numpy arrays and pandas
